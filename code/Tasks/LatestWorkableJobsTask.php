d8df7905ca50de487d48d6f5c46943781239691d
vinnie <vinnie_watson@hotmail.com>
2023-02-23 16:05:55 +1300

    Add Workable Job caching build task

---
+++ code/Tasks/LatestWorkableJobsTask.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SilverStripe\Workable\Tasks;
+
+use Psr\SimpleCache\CacheInterface;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Workable\Workable;
+
+class LatestWorkableJobsTask extends BuildTask
+{
+    /**
+     * @inheritdoc
+     */
+    protected $title = 'Refresh cache of Workable Jobs';
+
+    /**
+     * @inheritdoc
+     */
+    protected $description = 'Refresh cache of Workable Jobs';
+
+    /**
+     * @inheritdoc
+     */
+    public function run($request)
+    {
+        Workable::flush();
+
+        $params = ['state' => 'published'];
+        $jobs = singleton(Workable::class)->getFullJobs($params);
+
+        $output = "0 jobs to import";
+
+        if ($jobs && $jobs->count()) {
+            $cacheKey = 'FullJobs' . implode('-', $params);
+            $cache = Injector::inst()->get(CacheInterface::class . '.workable');
+
+            if ($cache->has($cacheKey)) {
+                $output = $jobs->count() . " total jobs saved successfully";
+            }
+        }
+
+        echo $output;
+
+        return;
+    }
+}
